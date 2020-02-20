f11f050523a756f3e78b0e2706237a9f10425b71
Ramon Smits <ramon.smits@gmail.com>
2020-02-20 23:34:57 +0100

    Added demo

---
+++ src/NServiceBus.Extensions.Logging.Demo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NServiceBus;
+using NServiceBus.Extensions.Logging;
+using NServiceBus.Logging;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
+
+public class Program
+{
+    public static async Task Main()
+    {
+        var loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder
+                .AddFilter("Microsoft", LogLevel.Warning)
+                .AddFilter("System", LogLevel.Warning)
+                .AddFilter("NServiceBus", LogLevel.Information)
+                .AddConsole();
+        });
+
+        LogManager.UseFactory(new ExtensionsLoggerFactory(loggerFactory));
+
+        var endpointConfiguration = new EndpointConfiguration("LoggingTest");
+
+        var instance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+        Console.ReadKey();
+        await instance.Stop()
+            .ConfigureAwait(false);
+
+    }
+}
