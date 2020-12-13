f2efed589fde51eaf0e1503db65b7bb9232ef9e2
Adam Ralph <adam@adamralph.com>
2020-12-13 18:35:25 +0100

    add failing test — AllPublicAsyncMethodsAcceptACancellationToken

---
+++ src/NServiceBus.Core.Tests/API/CancellationToken.cs
@@ -0,0 +1,33 @@
+﻿namespace NServiceBus.Core.Tests.API
+{
+    using NUnit.Framework;
+    using System.Linq;
+    using System.Reflection;
+    using System.Threading.Tasks;
+
+    class CancellationToken
+    {
+        [Test]
+        public void AllPublicAsyncMethodsAcceptACancellationToken()
+        {
+            var allTypes = typeof(IMessage).Assembly.GetTypes();
+
+            var publicTypes = allTypes
+                .Where(type => type.IsPublic || type.IsNestedPublic || type.IsNestedFamily || type.IsNestedFamORAssem).ToList();
+
+            var allMethods = publicTypes
+                .SelectMany(type => type.GetMethods()).ToList();
+
+            var allPublicMethods = allMethods
+                .Where(method => method.IsPublic || method.IsFamily || method.IsFamilyOrAssembly).ToList();
+
+            var taskReturningMethods = allPublicMethods
+                .Where(method => typeof(Task).IsAssignableFrom(method.ReturnType)).ToList();
+
+            var taskReturningMethodsMissingCancellationTokenParameter = taskReturningMethods
+                .Where(method => !method.GetParameters().Any(param => param.ParameterType == typeof(CancellationToken))).ToList();
+
+            Assert.IsEmpty(taskReturningMethodsMissingCancellationTokenParameter);
+        }
+    }
+}
